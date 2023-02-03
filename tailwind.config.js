c1c8796a0e01f133bf8dcdc12da8a18033671bff
ah-rasel <18103234@iubat.edu>
2023-02-03 15:58:55 +0600

    custom fonts and plugins added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ['*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      poppins: ['Poppins'],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+
 }
